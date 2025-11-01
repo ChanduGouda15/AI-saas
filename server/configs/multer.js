2309b49797ac66847ce1d8fb7ba8f8b95e62402d
chandu gouda <chandugouda15@gmail.com>
2025-11-01 12:48:45 +0530

    made some changes

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,5 +1,13 @@
-import multer from "multer";
+import multer from "multer"
 
-const storage = multer.diskStorage({});
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"./public")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,file.originalname)
+    }
+})
 
-export const upload = multer({storage})
+const upload=multer({storage})
+export default upload
